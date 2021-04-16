55d9af7cd9962338c3eaeb43130f73a929f0cc7c
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2021-04-16 17:47:49 -0600

    Main Menu

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.main_menu import *
+
+class Main:
+
+    def __init__(self):
+        MainMenu()
+
+Main()
---
+++ src/main_menu.py
@@ -0,0 +1,53 @@
+import time
+
+class MainMenu:
+
+    def __init__(self):
+        self.show_data()
+
+    def show_data(self):
+        print("Lenguajes formales y de programación A+")
+        print("Wilfred Alejandro Barrios Ola")
+        print("201602734")
+        self.counter_back()
+
+    def counter_back(self):
+        i = 5
+        while i > 0:
+            print(i)
+            i -= 1
+            time.sleep(1)
+        print("Bienvenido")
+        self.show_menu()
+
+    def show_menu(self):
+        print("")
+        print("1. Cargar archivo")
+        print("2. Mostrar información general de la gramática")
+        print("3. Generar autómata de pila equivalente")
+        print("4. Reporte de recorrido")
+        print("5. Reporte en tabla")
+        print("6. Salir")
+        print("> ", end='')
+        answer = input()
+        self.select_menu_option(answer)
+
+    def select_menu_option(self, option):
+
+        if option not in ["1", "2", "3", "4", "5", "6"]:
+            print("Seleccione una opción válida")
+            self.show_menu()
+            return
+
+        if option == "1":
+            print(option)
+        elif option == "2":
+            print(option)
+        elif option == "3":
+            print(option)
+        elif option == "4":
+            print(option)
+        elif option == "5":
+            print(option)
+        elif option == "6":
+            quit()
