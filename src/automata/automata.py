3bbd4fcd569eea07aacd23df15b0fe70062092b2
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2021-04-18 21:01:37 -0600

    Option 3 already finished

--- src/automata/automata.py
+++ src/automata/automata.py
@@ -6,7 +6,7 @@ class Automata:
     def build_equivalent_automata(self):
         grammar = ProyectoSingleton().selected_grammar
 
-        dot = Digraph(comment="AP_" + grammar.name, graph_attr={'rankdir':'LR'})
+        dot = Digraph(comment="AP_" + grammar.name, graph_attr={'rankdir':'LR', 'splines':'line'})
         dot.node("", "", color="white")
         dot.node("i", "i", shape='circle')
         dot.node("p", "p", shape='circle')
@@ -16,7 +16,22 @@ class Automata:
         dot.edge("", "i")
         dot.edge("i", "p", label="λ,λ;#")
         dot.edge("p", "q", label="λ,λ;" + grammar.io)
-        dot.edge("q", "f")
+
+        label = ""
+        for produccion in grammar.producciones:
+            rules = produccion["rules"]
+            for rule in rules:
+                label += "λ," + produccion["name"] + ";" + ','.join([str(val["valor"]) for val in rule]) + "\n"
+
+        dot.edge("q:n", "q",label=label)
+
+        label = ""
+        for out in grammar.terminales:
+            label += out + "," + out + ",λ\n"
+
+        dot.edge("q:s", "q", label=label)
+        
+        dot.edge("q", "f", label="λ,#;λ")
 
         dot.render('test-output/AP_' + grammar.name + '.gv', view=True)
         print("Autómata de pila equivalente generado exitósamente")
