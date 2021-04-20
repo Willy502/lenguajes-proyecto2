327bc49080b69377c8a02403075b635e1d852770
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2021-04-19 19:59:24 -0600

    Automata funcionando

--- src/automata/automata.py
+++ src/automata/automata.py
@@ -56,6 +56,8 @@ class Automata:
                 })
 
                 state = "p"
+
+                print(stack)
                 
             
             elif state == "p":
@@ -65,10 +67,69 @@ class Automata:
                 })
 
                 state = "q"
+                print(stack)
 
             elif state == "q":
-                print(stack)
-                return
+                for produccion in grammar.producciones:
+                    actual_stack_top = stack[0]
+                    #print (actual_stack_top)
+                    #print(produccion)
+                    if actual_stack_top["tipo"] == "no terminal" and actual_stack_top["valor"] == produccion["name"]:
+                        if len(produccion["rules"]) == 1:
+                            stack.pop(0)
+
+                            for o in reversed(range(len(produccion["rules"][0]))):
+                                stack.insert(0, produccion["rules"][0][o])
+
+                            print(stack)
+                            
+                            
+                        else:
+                            rule = {}
+                            current_char = string[i]
+                            for current_rule in produccion["rules"]:
+                                if current_rule[0]["valor"] == current_char:
+                                    rule = current_rule
+                                
+                            if rule == {}:
+                                #print("INTERNAL ---------")
+                                #print(current_char)
+                                #print(produccion)
+                                for int_rule in produccion["rules"]:
+                                    if int_rule[0]["valor"] in grammar.nterminales:
+                                        for int_prod in grammar.producciones:
+                                            if int_prod["name"] == int_rule[0]["valor"]:
+                                                for rule_searched in int_prod["rules"]:
+                                                    if rule_searched[0]["valor"] == current_char:
+                                                        rule = [{
+                                                            "tipo":"no terminal",
+                                                            "valor": int_rule[0]["valor"]
+                                                        }]
+
+                                #for internal_prod in grammar.producciones:
+                                    
+                                #print("-----------")
+
+                            stack.pop(0)
+
+                            for o in reversed(range(len(rule))):
+                                stack.insert(0, rule[o])
+
+                            print(stack)
+
+                    elif actual_stack_top["tipo"] == "terminal" and actual_stack_top["valor"] == string[i]:
+                        i += 1
+                        stack.pop(0)
+                        print(stack)
+
+                    elif actual_stack_top["tipo"] == "terminal" and actual_stack_top["valor"] != string[i]:
+                        print("error")
+                        return
+                    elif actual_stack_top["tipo"] == "no terminal" and actual_stack_top["valor"] == "#":
+                        stack.pop(0)
+                        state = "f"
+                        print(state)
+
 
             elif state == "f":
                 print("Estado f")
