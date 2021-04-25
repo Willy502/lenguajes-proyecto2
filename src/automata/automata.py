8494578abccad67a64e29e9ab42e5f85e12e4195
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2021-04-25 15:54:00 -0600

    Recorrido

--- src/automata/automata.py
+++ src/automata/automata.py
@@ -1,6 +1,7 @@
 from src.proyecto_singleton import *
 from graphviz import Digraph
 from common.helper import *
+import time
 
 class Automata:
 
@@ -38,6 +39,67 @@ class Automata:
         file_name = dot.filepath + ".pdf"
         Helper().build_html(grammar ,name_show ,file_name)
         print("Autómata de pila equivalente generado exitósamente")
+
+    def build_automata_running(self, data, gramatica):
+        transiciones = Helper().build_transiciones(data, gramatica)
+        label = ""
+        for transicion in transiciones:
+            if transicion != "(f)":
+                transicion = transicion[3:-1]
+                transicion = transicion.split(";")[0] + ";" + transicion.split(";")[1][2:]
+            else:
+                transicion = transicion[1:-1]
+            
+            print(transicion)
+            grammar = ProyectoSingleton().selected_grammar
+            name_show = "AP_" + grammar.name
+            dot = Digraph(comment=name_show, graph_attr={'rankdir':'LR', 'splines':'line'})
+            dot.node("", "", color="white")
+            dot.node("i", "i", shape='circle')
+            dot.node("p", "p", shape='circle')
+            dot.node("q", "q", shape='circle')
+            dot.node("f", "f", shape='doublecircle')
+
+            dot.edge("", "i")
+            label = "λ,λ;#"
+            if label == transicion:
+                label += " RUNNING"
+            dot.edge("i", "p", label=label)
+
+            label = "λ,λ;" + grammar.io
+            if label == transicion:
+                label += " RUNNING"
+            dot.edge("p", "q", label=label)
+
+            label = ""
+            for produccion in grammar.producciones:
+                rules = produccion["rules"]
+                for rule in rules:
+                    intern_label = "λ," + produccion["name"] + ";" + ''.join([str(val["valor"]) for val in rule])
+                    if intern_label == transicion:
+                        intern_label += " RUNNING"
+                    label += intern_label + "\n"
+
+            dot.edge("q:n", "q",label=label)
+
+            label = ""
+            for out in grammar.terminales:
+                intern_label = out + "," + out + ";λ"
+                if intern_label == transicion:
+                    intern_label += " RUNNING"
+                label += intern_label + "\n"
+
+            dot.edge("q:s", "q", label=label)
+            
+            label = "λ,#;λ"
+            if label == transicion:
+                label += " RUNNING"
+            dot.edge("q", "f", label=label)
+
+            dot.render('test-output/AP_' + grammar.name + '.gv', view=False)
+            file_name = dot.filepath + ".pdf"
+            Helper().build_html(grammar ,name_show ,file_name)
+            time.sleep(1)
 
     def run_report(self, string, menu_option):
         grammar = ProyectoSingleton().selected_grammar
@@ -185,7 +247,7 @@ class Automata:
                 })
                 print("Cadena aceptada")
                 if menu_option == 4:
-                    print("AJUA")
+                    self.build_automata_running(stack_to_print, grammar)
                 else:
                     Helper().build_table(stack_to_print, grammar)
                 return
